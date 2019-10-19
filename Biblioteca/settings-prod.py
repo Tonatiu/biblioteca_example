51ab20727c5046b02f665a00b899554b64f0e602
Tonatiu Trejo <yahuitl.trejo@gmail.com>
2019-10-19 11:55:30 -0500

    Se cambia el motor de base de datos

--- Biblioteca/settings-prod.py
+++ Biblioteca/settings-prod.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tonaWarrior$Biblioteca',
+        'USER': 'tonaWarrior',
+        'PASSWORD': '#Cor34ccE2z#',
+        'HOST': 'tonaWarrior.mysql.pythonanywhere-services.com',
     }
 }
 
